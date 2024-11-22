06dcda599bda6d2b098cc5ee142bea3523da4bb9
Rafael Rodrigues <106702893+RafaelSilva2k22@users.noreply.github.com>
2024-11-22 15:34:44 -0300

    Atualizar o layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -83,8 +83,8 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
-        <Footer />
         <CookieConsent />
+        <Footer />
       </body>
     </html>
   );
